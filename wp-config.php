cdbd0c0baa16fc61be223f4f5855f4ca3b18a780
EltraiN1337 <gilles.piechocki@gmail.com>
2018-11-04 13:15:50 +0100

    test gitlab/runcloud

--- wp-config.php
+++ wp-config.php
@@ -19,16 +19,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'landing-page-escort-v2');
+define('DB_NAME', 'portfolio');
 
 /** Utilisateur de la base de données MySQL. */
 define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'jEIijJtJM9AwV0KBHxiikH134nvbkXYI2EPJEzxO9NJaXvqTXQJ519e');
 
 /** Adresse de l’hébergement MySQL. */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define('DB_CHARSET', 'utf8mb4');
